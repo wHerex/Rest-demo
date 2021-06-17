9cba6b71a635fb5fe9c0eb8815de9c9f75e7f31a
marcin <mogrodniczak@nsa.gov.pl>
2021-06-18 01:03:37 +0200

    Hide unused setters from User class

--- src/main/java/com/wherex/rest/demo/model/History.java
+++ src/main/java/com/wherex/rest/demo/model/History.java
@@ -1,6 +1,5 @@
 package com.wherex.rest.demo.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
--- src/main/java/com/wherex/rest/demo/model/User.java
+++ src/main/java/com/wherex/rest/demo/model/User.java
@@ -1,17 +1,12 @@
 package com.wherex.rest.demo.model;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonView;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import java.time.LocalDate;
 
 @Getter
-@Setter
 @NoArgsConstructor
 public class User {
 
@@ -25,13 +20,15 @@ public class User {
 
     @JsonProperty("created_at")
     private LocalDate createdAt;
-    private double calculations ;
+    private double calculations;
 
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private double followers;
 
-    @JsonProperty(value= "public_repos", access = JsonProperty.Access.WRITE_ONLY)
+    @JsonProperty(value = "public_repos", access = JsonProperty.Access.WRITE_ONLY)
     private double publicRepos;
 
-
+    public void setCalculations(double calculations) {
+        this.calculations = calculations;
+    }
 }
